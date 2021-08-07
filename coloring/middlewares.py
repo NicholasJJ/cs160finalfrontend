1b388bee4fafbd8083fd05d4bceb41d4137f6b95
Nicholas Jennings <nicholasjennings@berkeley.edu>
2021-08-07 16:22:40 -0700

    d

--- coloring/middlewares.py
+++
@@ -1,6 +0,0 @@
-from django.utils.deprecation import MiddlewareMixin
-
-class MyTest(MiddlewareMixin):
-    def process_response(self, request, response):
-        response['Access-Control-Allow-Origin'] = "www.baidu.com"
-        return response
