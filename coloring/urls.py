e99161ad13e50eb8b486c9279d9555d624ae85f3
Emily Zhao <ezzhao@berkeley.edu>
2021-08-07 17:38:48 -0700

    added pages

--- coloring/urls.py
+++ coloring/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('gallery', views.gallery, name='gallery'),
-    path('testwrite', views.testwrite, name='testwrite'),
+    path('preview', views.preview, name='preview'),
+    path('add_story', views.add_story, name='add_story'),
+    path('write_summary', views.write_summary, name='write_summary'),
+    path('view_book', views.view_book, name='view_book'),
+    path('testwrite', views.testwrite, name='testwrite')
 ]
